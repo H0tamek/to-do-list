67179993306b4e271f7fd7cc50aa3fab5c917a6d
H0tamek <goosevbierqr@gmail.com>
2025-06-18 14:14:35 +0200

    Implemented core logic in task_manager.py

---
+++ task_manager.py
@@ -0,0 +1,25 @@
+# Класс для роботы с задачами
+
+class TaskManager:
+    def __init__(self, id, text, status):
+        self.id = id
+        self.text = text
+        self.status = status
+        
+    def view_tasks(self):
+        pass
+    
+    def add_task(self):
+        pass
+    
+    def edit_task(self):
+        pass
+    
+    def delete_task(self):
+        pass
+    
+    def mark_task_completed(self):
+        pass
+    
+    def mark_task_not_completed(self):
+        pass
